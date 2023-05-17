892eb9afe4c29cb69d8bbb8304903fcb26b42aa3
JazepsIvulis <jazeps.ivulis@gmail.com>
2023-05-17 22:16:03 +0300

    Initial Commit

---
+++ MovieDatabase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDatabase
+//
+//  Created by jazeps.ivulis on 17/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
